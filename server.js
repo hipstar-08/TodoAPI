fde47fe231e45c4d70e6d43774ba9418e95428e9
hipstar-08 <sonyjames9@gmail.com>
2017-10-29 22:21:22 +0530

    Add Put Todos

--- server.js
+++ server.js
@@ -55,6 +55,35 @@ app.delete('/todos/:id', function (req, res) {
   res.send('Asking for todo id of ' + req.params.id)
 });
 
+app.put('/todos/:id', function (req, res){
+  // var body = req.body;
+
+  var todoId = parseInt(req.params.id, 10);
+  var matchedTodo = _.findWhere(todos, {id: todoId});
+  var body = _.pick(req.body, 'description', 'completed');
+  var validAttributes = {};
+
+  if(!matchedTodo) {
+    return res.status(404).json({"error": "Not matched TODO, Hmm somethings wrong"});
+  }
+  if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+    validAttributes.completed =  body.completed;
+  } else  if (body.hasOwnProperty('completed')) {
+    return res.status(404).json({"error": "Hmm somethings wrong"});
+  }
+
+  if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+    validAttributes.description = body.description;
+  } else if (body.hasOwnProperty('description')) {
+    return res.status(400).send();
+  }
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+
+    _.extend(matchedTodo, validAttributes);
+    res.json(matchedTodo)
+});
+
 app.listen(PORT, function () {
   console.log('Express on Port: '+ PORT);
 });
