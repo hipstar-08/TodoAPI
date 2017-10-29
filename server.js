399d406709f9be541479ffa44fae6514a4795a80
hipstar-08 <sonyjames9@gmail.com>
2017-10-29 15:44:57 +0530

    Refactor with underscore

--- server.js
+++ server.js
@@ -1,20 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 var app = express();
 var PORT = process.env.ROOT || 4000;
-var todos = [{
-    id: 1,
-    description: 'Shifting tomorrow to new home, will work at Amazon and play at Kerala Blasters',
-    completed: false
-}, {
-    id: 2,
-    description: 'You will be at Amazon and play at Kerala Tuskers',
-    completed: false
-},  {
-    id: 3,
-    description: 'Sony you will be succesful at Amazon + Kerala Blasters',
-    completed: true
-}];
+var todos = [];
+var todoNextId = 1;
 
+app.use(bodyParser.json());
 app.get('/', function (req, res) {
   res.send('Todo API root');
 });
@@ -27,19 +20,25 @@ app.get('/todos', function (req, res) {
 app.get('/todos/:id', function (req, res) {
   // res.json(todos);
   var todoId = parseInt(req.params.id, 10);
-  var matchedTodo;
-
-  todos.forEach(function (todo) {
-    if (todoId === todo.id) {
-      matchedTodo = todo;
-    }
-  });
+  var matchedTodo = _.findWhere(todos, {id: todoId});
   if (matchedTodo) {
       res.json(matchedTodo);
   } else {
     res.status(404).send();
   }
   res.send('Asking for todo id of ' + req.params.id)
+});
+
+app.post('/todos', function (req, res){
+  // var body = req.body;
+  var body = _.pick(req.body, 'description', 'completed');
+  if (!_.isBoolean(body.completed) || !_.isString(body.description || body.description.trim().length === 0 )) {
+    return res.status(400).send();
+  }
+  body.description = body.description.trim();
+  body.id = todoNextId++;
+  todos.push(body);
+  res.json(body);
 });
 
 app.listen(PORT, function () {
