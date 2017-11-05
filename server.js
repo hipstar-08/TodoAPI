5513988fb7a45186413be86332ab1844a26e0783
hipstar-08 <sonyjames9@gmail.com>
2017-11-05 20:07:16 +0530

    GET method linked to sequelize db

--- server.js
+++ server.js
@@ -40,34 +40,26 @@ app.get('/todos', function (req, res) {
 });
 
 app.get('/todos/:id', function (req, res) {
-  // res.json(todos);
   var todoId = parseInt(req.params.id, 10);
-  var matchedTodo = _.findWhere(todos, {id: todoId});
-  if (matchedTodo) {
-      res.json(matchedTodo);
-  } else {
-    res.status(404).send();
-  }
-  res.send('Asking for todo id of ' + req.params.id)
+
+  db.todo.findById(todoId).then(function(todo) {
+    if (!!todo) {
+      res.json(todo.toJSON());
+    } else {
+      res.status(404).send();
+    }
+  }, function (e) {
+      res.status(500).send();
+  });
 });
 
 app.post('/todos', function (req, res){
-  // var body = req.body;
   var body = _.pick(req.body, 'description', 'completed');
-  // if (!_.isBoolean(body.completed) || !_.isString(body.description || body.description.trim().length === 0 )) {
-  //   return res.status(400).send();
-  // }
-  // body.description = body.description.trim();
-  // body.id = todoNextId++;
-  // todos.push(body);
-  // res.json(body);
-
   db.todo.create(body).then(function (todo) {
     res.json(todo.toJSON())
   }, function (e) {
       res.status(400).json(e);
   });
-
 });
 
 app.delete('/todos/:id', function (req, res) {
